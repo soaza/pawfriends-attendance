5bb36afa5f7522c3e7443529f0e4bb53c3783db3
soaza <65228562+soaza@users.noreply.github.com>
2022-02-22 14:26:30 +0800

    Add logic for updating

--- attendance_updater.py
+++ attendance_updater.py
@@ -1,20 +1,25 @@
 import csv
 from random import randint
+import pandas as pd
+from datetime import date
 
 
 def main():
 
    tracking_list = []
-   with open('BFF tracking sheet.csv') as csv_file:
+   
+   CSV_PATH = "BFF tracking sheet.csv"
+   
+   with open(CSV_PATH) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
-        if line_count == 0:
-            line_count += 1
-        else:
+        line_count += 1
+
+        if line_count >=1 :
             name,num_visits,last_visit,include = row
             if include:
-                new_person_obj = {"name":name,"num_visits": num_visits if num_visits else 0,"last_visit":last_visit,"random_tiebreaker":randint(0, 10)}
+                new_person_obj = {"index":line_count - 2,"name":name,"num_visits": num_visits if num_visits else 0,"last_visit":last_visit,"random_tiebreaker":randint(0, 10)}
                 tracking_list.append(new_person_obj)
                 
 
@@ -25,10 +30,22 @@ def main():
     # Get 5 members
     shortlisted_members = []
     for member in tracking_list[0:5]:
-        shortlisted_members.append(member["name"])
+        shortlisted_members.append((member["name"],member["index"]))
     
-    print(shortlisted_members)
-
+    print("Shortlisted:",shortlisted_members)
+    # reading the csv file
+    df = pd.read_csv(CSV_PATH)
+        
+    for member in shortlisted_members:
+        index = member[1]
+        df.loc[index,'Last Visit'] = date.today()
+        df.loc[index,'Number of Visits'] += 1
+        
+        
+    
+    # writing into the file
+    df.to_csv(CSV_PATH, index=False)
+    
     print(f'Processed {line_count} lines.')
 
 if __name__ == "__main__":
